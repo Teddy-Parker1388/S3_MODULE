cd6eb4aea8cc73713c728f67550a5e950fee83d2
Teddy Parker <57333590+Teddy-Parker1388@users.noreply.github.com>
2023-04-18 14:43:15 -0400

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,14 +1,14 @@
 output "bucket_ids"{
   description = "Name of bucket"
-  value = { for bucket_key,bucket_value in aws_s3_bucket.app_s3_bucket :  bucket_key => bucket_value.id }
+  value =  try(aws_s3_bucket.app_s3_bucket[0].id,null)
 }
 
 output "bucket_arns"{
   description = "ARN of the bucket"
-  value = { for bucket_key,bucket_value in aws_s3_bucket.app_s3_bucket :  bucket_key => bucket_value.arn }
+  value = try(aws_s3_bucket.app_s3_bucket[0].arn,null)
 }
 
 output "bucket_acl_id" {
   description = "The bucket, expected_bucket_owner (if configured), and acl (if configured) separated by commas (,)."
-  value = { for acl_key,acl_value in aws_s3_bucket_acl.app_bucket_acl :  acl_key => acl_value.id }
+  value = try(aws_s3_bucket_acl.app_bucket_acl[0].id,null)
 }
