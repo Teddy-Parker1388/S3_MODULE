b77782bf689c7ca961bd3cc78e828dfb7377f6cc
Teddy Parker <57333590+Teddy-Parker1388@users.noreply.github.com>
2023-04-19 01:36:34 -0400

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -8,6 +8,16 @@ output "bucket_arns"{
   value = try(aws_s3_bucket.app_s3_bucket[0].arn,null)
 }
 
+output "bucket_domain_name"{
+  description = "ARN of the bucket"
+  value = try(aws_s3_bucket.app_s3_bucket[0].bucket_domain_name,null)
+}
+
+output "bucket_regional_domain_name"{
+  description = "ARN of the bucket"
+  value = try(aws_s3_bucket.app_s3_bucket[0].bucket_regional_domain_name,null)
+}
+
 output "bucket_acl_id" {
   description = "The bucket, expected_bucket_owner (if configured), and acl (if configured) separated by commas (,)."
   value = try(aws_s3_bucket_acl.app_bucket_acl[0].id,null)
